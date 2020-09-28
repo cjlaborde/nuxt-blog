260c94b67316c4f829c8b06fd277754f0403cf89
Christian <cjlaborde@gmail.com>
2020-09-28 00:31:27 -0400

    Splitting the page into components

--- pages/index.vue
+++ pages/index.vue
@@ -4,40 +4,35 @@
       <h1>Get the latest tech news!</h1>
     </section>
     <section class="featured-posts">
-      <nuxt-link :to="'/posts/' + 1" class="post-preview">
-        <article>
-          <div
-            class="post-thumbnail"
-            style="
-              background-image: url(https://img.caixin.com/2019-07-24/1563971044321649.jpg);
-            "
-          ></div>
-          <div class="post-content">
-            <h1>Post Title</h1>
-            <p>Preview Text</p>
-          </div>
-        </article>
-      </nuxt-link>
-      <nuxt-link :to="'/posts/' + 2" class="post-preview">
-        <article>
-          <div
-            class="post-thumbnail"
-            style="
-              background-image: url(https://gnd-tech.com/content/2019/01/amd-ryzen-3-matisse.jpg);
-            "
-          ></div>
-          <div class="post-content">
-            <h1>Post Title 2</h1>
-            <p>Preview Text 2</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview
+        id="1"
+        thumbnail="https://img.caixin.com/2019-07-24/1563971044321649.jpg"
+        title="Hello there!"
+        previewText="This is my first post"
+      />
+      <PostPreview
+        id="2"
+        thumbnail="https://img.caixin.com/2019-07-24/1563971044321649.jpg"
+        title="Hello there! - the second time!"
+        previewText="This is my second post"
+      />
+      <PostPreview
+        id="3"
+        thumbnail="https://img.caixin.com/2019-07-24/1563971044321649.jpg"
+        title="Hi!"
+        previewText="This is my third post"
+      />
     </section>
   </div>
 </template>
 
 <script>
-export default {};
+import PostPreview from "@/components/Posts/PostPreview";
+export default {
+  components: {
+    PostPreview,
+  },
+};
 </script>
 
 <style scoped>
@@ -83,41 +78,5 @@ export default {};
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 </style>
