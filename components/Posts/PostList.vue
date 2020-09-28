74c25adac6b5bde238d87a9b86f9b3506e1708d5
Christian <cjlaborde@gmail.com>
2020-09-28 17:41:41 -0400

    Preparing the Backend

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,24 +2,40 @@
   <div>
     <PostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://img.caixin.com/2019-07-24/1563971044321649.jpg"
       title="Hello there!"
       previewText="This is my first post"
     />
     <PostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://img.caixin.com/2019-07-24/1563971044321649.jpg"
       title="Hello there! - the second time!"
       previewText="This is my second post"
     />
     <PostPreview
       id="3"
+      :is-admin="isAdmin"
       thumbnail="https://img.caixin.com/2019-07-24/1563971044321649.jpg"
       title="Hi!"
       previewText="This is my third post"
     />
   </div>
 </template>
+
+
+<script>
+export default {
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
 
 <style scoped>
 .posts-list {
