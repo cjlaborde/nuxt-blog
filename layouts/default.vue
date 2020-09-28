b7f7264b2c6a58bda155879412081079efb31450
Christian <cjlaborde@gmail.com>
2020-09-28 01:49:33 -0400

    Adding the Header component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,26 @@
 <template>
   <div>
+    <TheHeader @sideNavToggle="displaySideNav = !displaySideNav" />
+    <TheSideNav :show="displaySideNav" @close="displaySideNav = false" />
     <Nuxt />
   </div>
 </template>
+
+<script>
+import TheHeader from "@/components/Navigation/TheHeader";
+import TheSideNav from "@/components/Navigation/TheSideNav";
+export default {
+  components: {
+    TheHeader,
+    TheSideNav,
+  },
+  data() {
+    return {
+      displaySideNav: false,
+    };
+  },
+};
+</script>
 
 <style>
 html {
