9dee8d0abf9dc485f6c5e74013a172b8b73216ae
Christian <cjlaborde@gmail.com>
2020-09-30 07:29:16 -0400

    Vuex, fetch() and nuxtServerInit()

--- pages/index.vue
+++ pages/index.vue
@@ -8,44 +8,11 @@
 
 <script>
 export default {
-  // helps with SEO since it will not load page till data is loaded from server.
-  // executes on server.
-  // .this does not work here since asyncData works before this component actually created
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-      // console.log(context);
-      setTimeout(() =>
-        resolve(
-          {
-            loadedPosts: [
-              {
-                id: "1",
-                title: "First Post",
-                previewText: "This is our first post!",
-                thumbnail:
-                  "https://img.caixin.com/2019-07-24/1563971044321649.jpg",
-              },
-              {
-                id: "2",
-                title: "Second Post",
-                previewText: "This is our second post!",
-                thumbnail:
-                  "https://img.caixin.com/2019-07-24/1563971044321649.jpg",
-              },
-            ],
-          },
-          1500
-        )
-      );
-    });
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts;
+    },
   },
-  // this one one execute on the client
-  // data() {
-  //   return {
-  //     loadedPosts: [],
-  //   };
-  // },
-  created() {},
 };
 </script>
 
--- store/index.js
+++ store/index.js
@@ -11,6 +11,32 @@ const createStore = () => {
             }
         },
         actions: {
+            // Gets executed one time only on the server
+            // context is the payload that is always the same one you get in fetch and asyncData
+
+            nuxtServerInit(vuexContext, context) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        vuexContext.commit('setPosts', [
+                            {
+                                id: "1",
+                                title: "First Post",
+                                previewText: "This is our first post!",
+                                thumbnail:
+                                    "https://img.caixin.com/2019-07-24/1563971044321649.jpg",
+                            },
+                            {
+                                id: "2",
+                                title: "Second Post",
+                                previewText: "This is our second post!",
+                                thumbnail:
+                                    "https://img.caixin.com/2019-07-24/1563971044321649.jpg",
+                            },
+                        ])
+                        resolve();
+                    }, 1000)
+                });
+            },
             setPosts(vuexContext, posts) {
                 vuexContext.commit('setPosts', posts)
             }
