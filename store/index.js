c8323ac29147d19e42cec4ea780080aa10c08c84
Christian <cjlaborde@gmail.com>
2020-09-30 11:42:30 -0400

    Convert to Async function

--- store/index.js
+++ store/index.js
@@ -14,20 +14,21 @@ const createStore = () => {
         actions: {
             // Gets executed one time only on the server to have fast loading times
             // context is the payload that is always the same one you get in fetch and asyncData
-            nuxtServerInit(vuexContext, context) {
-                return axios.get('https://nuxt-blog-bee7d.firebaseio.com/posts.json')
-                    .then(res => {
-                        const postsArray = []
-                        for (const key in res.data) {
-                            // you can store the actual id by pushing a new object
-                            // where we use spread operator to pull out all the properties
-                            // we have in that data object were fetching for a given key
-                            // this can be used if we later want to edit the post
-                            postsArray.push({ ...res.data[key], id: key })
-                        }
-                        vuexContext.commit('setPosts', postsArray)
-                    })
-                    .catch(e => context.error(e))
+            async nuxtServerInit(vuexContext, context) {
+                try {
+                    const res = await axios.get('https://nuxt-blog-bee7d.firebaseio.com/posts.json');
+                    const postsArray = [];
+                    for (const key in res.data) {
+                        // you can store the actual id by pushing a new object
+                        // where we use spread operator to pull out all the properties
+                        // we have in that data object were fetching for a given key
+                        // this can be used if we later want to edit the post
+                        postsArray.push({ ...res.data[key], id: key });
+                    }
+                    vuexContext.commit('setPosts', postsArray);
+                } catch (e) {
+                    return context.error(e);
+                }
             },
             setPosts(vuexContext, posts) {
                 vuexContext.commit('setPosts', posts)
